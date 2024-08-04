3f22507649e8de298109165293c19d9e00c5db6e
ezekiel-campos <ezekielcampos@Ezekiels-MacBook-Pro.local>
2024-08-04 13:01:13 -0700

    created route and handlebar page to write a post

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+
+const {Post} = require('../../models')
+
+const withAuth = require('../../middleware/auth')
+
+router.post('/', withAuth, async(res, req) =>{
+
+    try {
+        const postData = await Post.create({
+            ...req.body,
+            user_id: req.session.user_id,
+          });
+
+          res.status(200).json(postData)
+
+
+    } catch (error) {
+        res.status(400).json(err);
+    }
+
+  
+
+
+
+
+
+
+})
+
+module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,37 +1,34 @@
-const router = require('express').Router()
+const router = require('express').Router();
+const withAuth = require('../middleware/auth')
 
+router.get('/', (req, res) => {
+  try {
+    res.render('homepage', { logged_in: req.session.logged_in });
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
+});
 
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
 
-router.get('/', (req, res)=>{
+  res.render('login', { logged_in: req.session.logged_in });
+});
 
-    try {
-        res.render('homepage', {logged_in:req.session.logged_in})
-    } catch (error) {
-      console.error('An error occurred:', error);
-    }
+router.get('/signup', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
 
-})
+  res.render('new-user', { logged_in: req.session.logged_in });
+});
 
+router.get('/create', withAuth, (req, res) => {
+  res.render('new-post',{ logged_in: req.session.logged_in });
+});
 
-router.get('/login', (req, res)=>{
-
-    if (req.session.logged_in) {
-        res.redirect('/');
-        return;
-      }
-    
-      res.render('login', {logged_in:req.session.logged_in});
-})
-
-router.get('/signup', (req, res)=>{
-
-    if (req.session.logged_in) {
-        res.redirect('/');
-        return;
-      }
-    
-      res.render('new-user', {logged_in:req.session.logged_in});
-})
-
-
-module.exports = router
+module.exports = router;
