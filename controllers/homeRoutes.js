105deba8049566e55719224653d4069ae233ae78
ezekiel-campos <ezekielcampos@Ezekiels-MacBook-Pro.local>
2024-08-04 23:15:16 -0700

    created home route that will update specific post from the user dashboard

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,9 +3,8 @@ const withAuth = require('../middleware/auth');
 
 const { Post, User } = require('../models');
 
-router.get('/', async(req, res) => {
+router.get('/', async (req, res) => {
   try {
-
     console.log(req.session.user_id);
     const userPosts = await Post.findAll({
       include: [
@@ -16,7 +15,7 @@ router.get('/', async(req, res) => {
       ],
     });
 
-    const posts = userPosts.map((post)=>post.get({plain:true}))
+    const posts = userPosts.map((post) => post.get({ plain: true }));
     console.log(posts);
 
     res.render('homepage', { posts, logged_in: req.session.logged_in });
@@ -46,24 +45,30 @@ router.get('/create', withAuth, (req, res) => {
   res.render('new-post', { logged_in: req.session.logged_in });
 });
 
-router.get('/dashboard', withAuth, async(req, res) => {
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    const userPosts = await Post.findAll({
+      where: {
+        user_id: req.session.user_id,
+      },
+    });
 
-try {
+    const list = userPosts.map((post) => post.get({ plain: true }));
 
-   const userPosts = await Post.findAll({where:{
-    user_id:req.session.user_id
-  }})
+    console.log(list);
 
-  const list = userPosts.map((post)=>post.get({plain:true}))
-  
-  console.log(list)
-  
-  res.render('dashboard', { list, logged_in: req.session.logged_in });
-} catch (error) {
-  
-}
+    res.render('dashboard', { list, logged_in: req.session.logged_in });
+  } catch (error) {}
+});
 
-  
+router.get('/modify/:id', async (req, res) => {
+  try {
+
+    const specificPost = await Post.findByPk(req.params.id, {raw:true});
+   console.log(specificPost);
+    res.render('update-post', {...specificPost, logged_in: req.session.logged_in })
+
+  } catch (error) {}
 });
 
 module.exports = router;
