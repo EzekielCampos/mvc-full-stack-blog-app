ead08914aab681d554550afb7a178c467a98c426
ezekiel-campos <ezekielcampos@Ezekiels-MacBook-Pro.local>
2024-08-05 22:44:29 -0700

    in comment.js file created function to extract the input from  the user comment

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,2 +1,21 @@
 
 
+const submitComment = async(event) =>{
+
+
+    const comment = document.querySelector('#user-comment').value.trim();
+
+    if(comment){
+
+        
+    }
+
+   
+
+
+}
+
+
+
+
+document.querySelector('#comment-frm').addEventListener('submit', submitComment)
