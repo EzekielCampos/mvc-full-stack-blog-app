6121dddae666d71977a7ca1dd54093c612cac823
ezekiel-campos <ezekielcampos@Ezekiels-MacBook-Pro.local>
2024-08-08 08:24:51 -0700

    created a env port associataed with  PostgreSQL

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,12 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-let sequelize;
 
-// This will create the connection to the database with credentials
-if (process.env.DB_URL) {
-  sequelize = new Sequelize(process.env.DB_URL);
-} else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: 'localhost',
     dialect: 'postgres',
+    port: parseInt(process.env.DB_PORT, 10),
   });
-}
+
 
 module.exports = sequelize;
