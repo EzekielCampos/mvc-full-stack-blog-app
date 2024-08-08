5761ef8ae007ba83fb9b133fdea8f6b06dc3b58e
ezekiel-campos <ezekielcampos@Ezekiels-MacBook-Pro.local>
2024-08-08 00:28:11 -0700

    changed layout for connection

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,15 @@
-const Sequelize = require('sequelize');
 require('dotenv').config();
 
-let sequelize;
-
-// This will create the connection to the database with credentials
-if (process.env.DB_URL) {
-  sequelize = new Sequelize(process.env.DB_URL);
-} else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: 'localhost',
-    dialect: 'postgres',
-  });
-}
+const Sequelize = require('sequelize');
+// This will give access to the database and automatically update 
+const sequelize = process.env.DB_URL
+  ? new Sequelize(process.env.DB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: 'localhost',
+      dialect: 'postgres',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
 
 module.exports = sequelize;
