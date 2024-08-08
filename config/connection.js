bfc605dd40c273bc10aed32ce7186045436aa776
ezekiel-campos <ezekielcampos@Ezekiels-MacBook-Pro.local>
2024-08-08 00:32:30 -0700

    sequelize connection changed

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,13 @@
 require('dotenv').config();
 
 const Sequelize = require('sequelize');
-// This will give access to the database and automatically update 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'postgres',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+// This will give access to the database and automatically update
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: 'localhost',
+  dialect: 'postgres',
+  dialectOptions: {
+    decimalNumbers: true,
+  },
+});
 
 module.exports = sequelize;
